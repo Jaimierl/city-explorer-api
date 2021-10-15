d973075a138ea4d34fa98af4ecace1374d2d0308
Jaimierl <jaimieloney@gmail.com>
2021-10-14 21:52:41 -0400

    Class changes coming

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
 const axios = require('axios');
 
 const app = express();
